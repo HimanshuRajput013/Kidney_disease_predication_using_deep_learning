2bc632d9806a7a1886747c293f2dd13afaef144c
HimanshuRajput013 <hrajput0013@gmail.com>
2024-06-26 00:02:02 +0530

    training

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
